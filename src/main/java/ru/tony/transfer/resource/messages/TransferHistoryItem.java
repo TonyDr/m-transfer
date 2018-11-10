06da0b3f317e55e6c09fdd28e64d8a21f68314bd
Anton Dranitsyn <t0t0shc0@gmail.com>
2018-11-10 23:16:16 +0300

    Not include null fields

--- src/main/java/ru/tony/transfer/resource/messages/TransferHistoryItem.java
+++ src/main/java/ru/tony/transfer/resource/messages/TransferHistoryItem.java
@@ -1,15 +1,19 @@
 package ru.tony.transfer.resource.messages;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.Date;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @Getter
 @Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(NON_NULL)
 public class TransferHistoryItem {
 
     private Long transactionId;
